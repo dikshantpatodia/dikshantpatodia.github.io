f4f53ba5affb3832119f9201a5e986e20cf4ac56
Dikshant Patodia <dikshantpatodia@gmail.com>
2017-03-22 15:04:08 +0530

    Add files via upload

---
+++ projects/freelance_projects/js/my.js
@@ -0,0 +1,12 @@
+$(function () {
+var d = new Date();
+document.getElementById("demo").innerHTML = d.toDateString();
+});
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+});
+
+$(function () {
+document.getElementById("element").innerHTML = '../footer.html'
+});
